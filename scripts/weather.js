a1705084f9e3f81f751a2dbb65852409f383ae46
dinismubai <mubydinis@gmail.com>
2024-11-22 20:37:58 +0200

    Country activity

---
+++ scripts/weather.js
@@ -0,0 +1,11 @@
+let temperature = 30;/*parseInt(document.getElementById("currenttemp").innerText);*/
+let windspeed = 6;/*parseFloat(document.getElementById("speed").innerText);*/
+
+let factor = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temperature * Math.pow(windspeed, 0.16);
+
+if (temperature <= 10 && windspeed > 4.8) {
+    document.getElementById("chill").innerHTML = Math.round(factor);
+}
+else {
+    document.getElementById("chill").innerHTML = "N/A";
+}
