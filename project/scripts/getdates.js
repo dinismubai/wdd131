85442412c45eb4da0c00267ddf84e333d8a4c4ec
dinismubai <mubydinis@gmail.com>
2024-12-04 14:32:22 +0200

    Site plan

---
+++ project/scripts/getdates.js
@@ -0,0 +1,6 @@
+let lastmodified = document.lastModified;
+document.getElementById("lastmodified").innerHTML = lastmodified
+
+const d = new Date();
+    let year = d.getFullYear();
+    document.getElementById("year").textContent = year;
